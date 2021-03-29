6bfa24e525b6cf3cbf8d5d993b225b692415c961
patryk.mauer <patryk.mauer@student.po.edu.pl>
2021-03-29 11:13:49 +0200

    no_commission sign works

--- listings/models.py
+++ listings/models.py
@@ -9,6 +9,7 @@ from realtors.models import Realtor
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     category = models.CharField(max_length=100, choices=[('Mieszkanie', 'Mieszkanie'), ('Dom', 'Dom'), ('Działka','Działka'),('Lokal','Lokal')])
+    commission = models.BooleanField()
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
