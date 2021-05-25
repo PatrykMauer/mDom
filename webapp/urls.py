8d4db20471692707c857cd71a751e433eb2cf53d
patryk.mauer <patryk.mauer@student.po.edu.pl>
2021-05-25 22:44:34 +0200

    load static added and commented out urls to migrate the database

--- webapp/urls.py
+++ webapp/urls.py
@@ -19,11 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    # path('admin/', admin.site.urls),
+    # path('', include('pages.urls')),
     # path('listings/', include('listings.urls')),
-    path('accounts/', include('accounts.urls')),
-     path('contacts/', include('contacts.urls')),
+    # path('accounts/', include('accounts.urls')),
+    #  path('contacts/', include('contacts.urls')),
      path('publish/', include('publish.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
