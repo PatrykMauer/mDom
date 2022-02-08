e0241d4a2a51173fa0c150b8e2f3fb4e5e17401a
patryk.mauer <patryk.mauer@student.po.edu.pl>
2022-02-08 12:33:50 +0100

    removed www.mdom and mdom

--- webapp/settings.py
+++ webapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost, www.mdom-nieruchomosci.pl, mdom-nieruchomosci.pl").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost").split(",")
 
 
 # Application definition
